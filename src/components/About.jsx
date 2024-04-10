a6791aaf49e8eafabc89c61ad73d0671ff1f491d
ShivamChansoria <chansoriyashivam@gmail.com>
2024-04-10 20:10:12 +0530

    minor-touch

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -40,7 +40,7 @@ const About = () => {
      </motion.div>
      <motion.p variants={fadeIn("", "", 0.1, 1)}
      className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]' >
-      I'm a fresher software developer skilled in C++, JavaScript, and having experience in frameworks like React, Node.js, and Bootstrap. I'm a quick learner and can create efficient scalable, and user-friendly software solutions  that solve real world problems. Excited to work into the real production environment to bring the ideas to life!
+      I'm a fresher software developer skilled in C++, JavaScript, and having experience in frameworks like React, MongoDB, Node.js, ExpressJS, SQL, GraphQL, TypeScript and Bootstrap. I'm a quick learner and can create efficient scalable, and user-friendly software solutions  that solve real world problems. Excited to work into the real production environment to bring the ideas to life!
      </motion.p>
      <div className='mt-20 flex flex-wrap gap-10'>
       {services.map((service, index) => (
